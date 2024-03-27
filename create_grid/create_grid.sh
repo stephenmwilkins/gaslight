dfe726fee0ad4e9d7e85e7bbeb5bb5fd7db32851
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-03-27 09:34:20 +0000

    updated pipeline

--- create_grid/create_grid.sh
+++ create_grid/create_grid.sh
@@ -1,9 +1,9 @@
 
-incident_grid_dir='/Users/sw376/Dropbox/Research/data/synthesizer/grids'
-grid_dir='/Users/sw376/Dropbox/Research/data/gaslight/grids'
+incident_grid_dir='/research/astrodata/highz/synthesizer/grids'
+grid_dir='/research/astrodata/highz/gaslight/grids'
 incident_grid='bpass-2.2.1-bin_chabrier03-0.1,300.0-ages:6.,7.,8.'
 config_file='c23.01-reduced'
-output_dir='/Users/sw376/Dropbox/Research/data/gaslight/cloudy'
+output_dir='/research/astrodata/highz/gaslight/cloudy'
 
 # run the setup script
 python create_grid.py -grid_dir=$grid_dir -incident_grid_dir=$incident_grid_dir -incident_grid=$incident_grid -config_file=$config_file -output_dir=$output_dir
