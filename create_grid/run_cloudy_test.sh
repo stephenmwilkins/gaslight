dd0aff93032c761b7f09e16768c7fa3d02fd4edd
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-03-27 13:17:05 +0000

    updated test

--- create_grid/run_cloudy_test.sh
+++ create_grid/run_cloudy_test.sh
@@ -11,7 +11,7 @@ cloudy_dir='/research/astro/flare/software/cloudy/'
 # run the setup script
 python setup_grid_creation.py -grid_dir=$grid_dir -incident_grid=$incident_grid -config_file=$config_file -output_dir=$output_dir -cloudy_dir=$cloudy_dir -index=$index
 
-# now run a single grid point
-python run_cloudy.py -grid_dir=$grid_dir -incident_grid=$incident_grid -config_file=$config_file -output_dir=$output_dir -cloudy_dir=$cloudy_dir -index=$index
+# # now run a single grid point
+# python run_cloudy.py -grid_dir=$grid_dir -incident_grid=$incident_grid -config_file=$config_file -output_dir=$output_dir -cloudy_dir=$cloudy_dir -index=$index
 
 
