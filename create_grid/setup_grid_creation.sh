4edc445de4e41b4ffbf558b5ef7287c230994678
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-03-29 10:36:59 +0000

    updated pipeline

--- create_grid/setup_grid_creation.sh
+++ create_grid/setup_grid_creation.sh
@@ -12,12 +12,12 @@ cloudy_dir='/research/astro/flare/software/cloudy/'
 # cloudy_dir='/Users/sw376/Dropbox/Research/software/cloudy'
 
 # standard SPS grid
-# incident_grid='bpass-2.2.1-bin_chabrier03-0.1,300.0-ages:6.,7.,8.'
-# config_file='c23.01-full'
+incident_grid='bpass-2.2.1-bin_chabrier03-0.1,300.0-ages:6.,7.,8.'
+config_file='c23.01-full'
 
 # standard AGN grid
-incident_grid='agnsed-limited'
-config_file='c23.01-agn-limited'
+# incident_grid='agnsed-limited'
+# config_file='c23.01-agn-limited'
 
 # run the setup script
 python setup_grid_creation.py -grid_dir=$grid_dir -incident_grid=$incident_grid -config_file=$config_file -output_dir=$output_dir -cloudy_dir=$cloudy_dir
