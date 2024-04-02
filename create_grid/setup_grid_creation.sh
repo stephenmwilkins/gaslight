d293c20205271ddb044014e31e6c341713798daa
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-04-02 07:44:03 +0100

    updated

--- create_grid/setup_grid_creation.sh
+++ create_grid/setup_grid_creation.sh
@@ -21,7 +21,7 @@ incident_grid='agnsed-isotropic.hdf5'
 config_file='c23.01-agn-nlr'
 
 # run the setup script
-python setup_grid_creation.py -grid_dir=$grid_dir -incident_grid=$incident_grid -config_file=$config_file -output_dir=$output_dir -cloudy_dir=$cloudy_dir
+python setup_grid_creation.py -grid_dir=$grid_dir -incident_grid=$incident_grid -config_file=$config_file -output_dir=$output_dir -cloudy_dir=$cloudy_dir -machine=$machine
 
 # index=1
 
