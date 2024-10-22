8bfd8296feaa1f063dd6259e06f914f0df0e3c2a
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2024-10-22 14:03:16 +0100

    added error capture

--- create_grid/create_gaslight_grid.py
+++ create_grid/create_gaslight_grid.py
@@ -2,6 +2,7 @@ import h5py
 import argparse
 import pickle
 import numpy as np
+from pathlib import Path
 from synthesizer.grid import Grid
 from utils import (
     get_grid_properties,
@@ -69,8 +70,6 @@ if __name__ == "__main__":
         read_lines=False,
     )
 
-
-
     # get properties of the photoionsation grid
     (
         photoionisation_n_axes,
@@ -137,36 +136,56 @@ if __name__ == "__main__":
     for line_id in line_ids:
         luminosity[line_id] = np.empty(total_shape)
 
+    failed_grid_points = []
+
     for i, photoionisation_index in enumerate(photoionisation_index_list):
-        # open pickle file
-        with open(f'{output_directory}/{i}.pck', 'rb') as file:
-            out = pickle.load(file)
 
-            # loop over lines
+        # Check to see if pickle file exists. If not append to list of failed
+        # grid points.
+
+        if not Path(f'{output_directory}/{i}.pck').is_file():
+            failed_grid_points.append(i)
+
+        # If any models have failed, keep looping but don't bother trying to
+        # read the files.
+        if len(failed_grid_points) == 0:
+
+            # Open pickle file
+            with open(f'{output_directory}/{i}.pck', 'rb') as file:
+                out = pickle.load(file)
+
+                # loop over lines
+                for line_id in line_ids:
+
+                    # loop over incident models
+                    for incident_index in incident_index_list:
+                        
+                        # full index
+                        # print(photoionisation_index, incident_index)
+
+                        index = (tuple(list(incident_index)
+                                    + list(photoionisation_index)))
+                        luminosity[line_id][index] = out[line_id][tuple(incident_index)]
+
+    # If there are failures list them here:
+    if len(failed_grid_points) > 0:
+        print(failed_grid_points)
+
+        # Save this list and generate a new run command
+
+    # open the new grid and save results if there are not failures
+    if len(failed_grid_points) == 0:
+        with h5py.File(f"{grid_dir}/{model_name}.hdf5", "w") as hf:
+
+            # save a list of the axes in the correct order
+            hf.attrs['axes'] = total_axes
+
+            # save the values of the axes
+            for k, v in total_axes_values.items():
+                hf[f'axes/{k}'] = v
+
             for line_id in line_ids:
+                hf[f'luminosity/{line_id}'] = luminosity[line_id]
 
-                # loop over incident models
-                for incident_index in incident_index_list:
-                    
-                    # full index
-                    # print(photoionisation_index, incident_index)
-
-                    index = (tuple(list(incident_index)
-                                   + list(photoionisation_index)))
-                    luminosity[line_id][index] = out[line_id][tuple(incident_index)]
-
-    # open the new grid and save results
-    with h5py.File(f"{grid_dir}/{model_name}.hdf5", "w") as hf:
-
-        # save a list of the axes in the correct order
-        hf.attrs['axes'] = total_axes
-
-        # save the values of the axes
-        for k, v in total_axes_values.items():
-            hf[f'axes/{k}'] = v
-
-        for line_id in line_ids:
-            hf[f'luminosity/{line_id}'] = luminosity[line_id]
-
-        # print
-        hf.visit(print)
+            # print
+            hf.visit(print)
